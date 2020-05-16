ae1d87c32d487c5cc6039926665cb2c7765162ba
Arnold Angerer <arnold.angerer@gmail.com>
2020-05-16 20:10:09 +0200

    eager loading - end of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,14 +1,12 @@
-﻿using Infrastructure.Data;
-using Core.Entities;
+﻿using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Interfaces;
 
 namespace API.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
@@ -27,6 +25,19 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {            
             return await _repo.GetProductByIdAsync(id) ;
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {            
+            var productBrands = await _repo.GetProductBrandsAsync();
+            return Ok(productBrands);            
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {            
+            var productTypess = await _repo.GetProductTypesAsync();
+            return Ok(productTypess);            
         }
     }
 }
